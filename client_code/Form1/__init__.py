534a5028cd0648022f3ebd0f487d5a237074196d
paliwaldg@gmail.com <paliwaldg@gmail.com>
2024-07-08 12:58:31 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class Form1(Form1Template):
@@ -8,3 +12,11 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    email = self.email.text
+    password = self.password.text
+    anvil.server.call('submit', Email = email, password = password)
+    Notification("Signed In successfully").show()
+
