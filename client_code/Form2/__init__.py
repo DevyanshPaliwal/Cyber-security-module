96fb4ac010accee3905b53d39359373eea933c2b
paliwaldg@gmail.com <paliwaldg@gmail.com>
2024-07-09 14:33:24 +0000

    Edited Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -12,3 +12,11 @@ class Form2(Form2Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    mail = self.text_box_1.text
+    pwd = self.text_box_2.text
+    if mail == app_tables.data.search(mail) and pwd == app_tables.data.search(pwd):
+      
+      
